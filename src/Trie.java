3c1ea178df044862f9c91cd4135bed7304f14168
marekkubu <marekkubu@gmail.com>
2016-11-17 13:24:54 +0100

    trie začatek

--- src/Trie.java
+++ src/Trie.java
@@ -1,5 +1,26 @@
+import java.lang.ref.Reference;
+
 /**
  * Created by Marek on 17. 11. 2016.
  */
 public class Trie {
+    String[] edges= new String[25];
+
+    public void addWord(Vertex vrchol, String word){
+        if (word.isEmpty() == true) {
+            vrchol.words=vrchol.words+1;
+        }
+        else {
+            vrchol.prefixes=vrchol.prefixes+1;
+            char k = word.charAt(0);
+
+            for (int i = 0; i < edges.length; i++) {
+
+             if (edges[i].equals(k)==false) {
+
+             }
+            }
+        }
+
+    }
 }
