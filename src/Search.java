98a6ee6590a499b308420c85154027a592e57c72
marekkubu <marekkubu@gmail.com>
2016-11-17 10:03:17 +0100

    Odstranění nepotřebné metody

--- src/Search.java
+++ src/Search.java
@@ -13,35 +13,9 @@ public class Search {
     Dictionary dictionary = new Dictionary();
 
     /**
-     * Metoda pro vyhledávání zadaného slova. Vypíše a uloží nalezená slova do soubouru. Včetně jejich indexu.
-     * @param searchWord
+     * Metoda pro vyhledávání zadaného slova. Vypíše a uloží nalezená slova do soubouru. Včetně jejich počátečního a koncového indexu.
+     * @param wordToFind
      */
-    public void searching(String searchWord) {
-
-        try {
-            dictionary.createArray();
-            UserInterface.BuffWriter = new BufferedWriter(new FileWriter("searchOut.txt"));
-            for (int i = 0; i < Dictionary.words.length; i++) {
-                if (searchWord.equals(Dictionary.words[i])) {
-                    int index = i + 1;
-                    String foundWord = Dictionary.words[i];
-                    System.out.println("Searched word '" + foundWord + "' index: " + index);
-                    UserInterface.BuffWriter.write("Searched word '" + foundWord + "' index: " + index);
-                    UserInterface.BuffWriter.newLine();
-                }
-            }
-
-        } catch (IOException e) {
-        } finally {
-            try {
-                if (UserInterface.BuffWriter != null) {
-                    UserInterface.BuffWriter.close();
-                }
-            } catch (IOException e) {
-            }
-        }
-
-    }
 
     public void indexSearching(String wordToFind){
         String str = UserInterface.textArea.getText().toLowerCase();
